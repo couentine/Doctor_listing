fd1c215de69bb2a498366772e9cc2f8eb365b836
Quentin <quent.guillaume@gmail.com>
2018-06-28 21:55:38 -0700

    search

--- Gemfile
+++ Gemfile
@@ -8,7 +8,8 @@ end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
-
+gem 'elasticsearch-model'
+gem 'elasticsearch-rails'
 #devise
 gem 'devise'
 #author
--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,4 +1,6 @@
 class Doc < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
   def self.search(search)
     where("name ILIKE ? OR speciality ILIKE ? OR zipc ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
   end
