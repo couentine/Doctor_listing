7f592d3c2975763c22d3229df2cc8e97be3f3397
Quentin <quent.guillaume@gmail.com>
2018-06-28 20:23:16 -0700

    heroku testing

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,5 +1,5 @@
 class Doc < ApplicationRecord
   def self.search(search)
-    where("name LIKE ? OR speciality LIKE ? OR zipc LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
+    where("name::text LIKE ? OR speciality::text LIKE ? OR zipc::integer LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
   end
 end
